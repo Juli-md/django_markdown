bf4ae513005bf59d07a9e1adeefea6b4f61d0769
Juliya Medvedeva <juliamd928@gmail.com>
2021-02-26 18:55:01 +1000

    fix for django upgrade

--- django_markdown/templatetags/django_markdown_static.py
+++ django_markdown/templatetags/django_markdown_static.py
@@ -4,10 +4,15 @@ from django.template import Library
 register = Library()
 
 if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
-    from django.contrib.staticfiles.templatetags.staticfiles import static as _static
+    try:
+        # Django 2
+        from django.contrib.staticfiles.templatetags.staticfiles import static as _static
+    except ModuleNotFoundError:
+        # Django 3
+        from django.templatetags.static import static
 else:
     from django.templatetags.static import static as _static
-
+    
 
 @register.simple_tag
 def static(path):
